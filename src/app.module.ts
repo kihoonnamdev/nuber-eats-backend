b0d41d7a3833ba4c9ead01d167fdcd4607b5c15d
kihoonnamdev <kihoon.nam.dev@gmail.com>
2024-04-02 09:50:31 +0900

    2.1~2.4 GRAPHQL API

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+//import { join } from 'path';
+import { RestaurantsModule } from './restaurants/restaurants.module';
+import { ApolloDriver } from '@nestjs/apollo';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    RestaurantsModule,
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
