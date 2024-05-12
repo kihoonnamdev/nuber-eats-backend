ca5562dfdbf86ef2030985dd2ec0492bee0dfa04
kihoonnamdev <kihoon.nam.dev@gmail.com>
2024-05-12 11:56:23 +0900

    5.9 Log In part Two

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { CreateAccountInput } from './dtos/create-account.dto';
+import { LoginInput } from './dtos/login.dto';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
@@ -26,6 +27,38 @@ export class UserService {
     } catch (e) {
       //make error
       return { ok: false, error: "Couldn't create account" };
+    }
+  }
+
+  async login({
+    email,
+    password,
+  }: LoginInput): Promise<{ ok: boolean; error?: string; token?: string }> {
+    // make a JWT and give it to the user
+    try {
+      const user = await this.users.findOne({ where: { email } });
+      if (!user) {
+        return {
+          ok: false,
+          error: 'User not found',
+        };
+      }
+      const passwordCorrect = await user.checkPassword(password);
+      if (!passwordCorrect) {
+        return {
+          ok: false,
+          error: 'Wrong password',
+        };
+      }
+      return {
+        ok: true,
+        token: 'lalalalaalala',
+      };
+    } catch (error) {
+      return {
+        ok: false,
+        error,
+      };
     }
   }
 }
