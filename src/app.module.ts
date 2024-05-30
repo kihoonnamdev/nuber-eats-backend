dc31e6031821f2aea727dbcd674d09e53a26153b
kihoonnamdev <kihoon.nam.dev@gmail.com>
2024-05-31 06:22:09 +0900

    7.0 Verification Entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,6 +18,7 @@ import { JwtModule } from './jwt/jwt.module';
 import { JwtMiddleware } from './jwt/jwt.middleware';
 //import { CommonModule } from './common/dtos/common.module';
 import { AuthModule } from './auth/auth.module';
+import { Verification } from './users/entities/verification.entity';
 
 @Module({
   imports: [
@@ -44,7 +45,7 @@ import { AuthModule } from './auth/auth.module';
       database: process.env.DB_NAME,
       synchronize: process.env.NODE_ENV !== 'prod',
       logging: process.env.NODE_ENV !== 'prod',
-      entities: [User],
+      entities: [User, Verification],
     }),
     GraphQLModule.forRoot({
       driver: ApolloDriver,
