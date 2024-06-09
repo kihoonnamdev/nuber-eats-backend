e01355ac585719668b23eae6c58e1a7aefa87948
kihoonnamdev <kihoon.nam.dev@gmail.com>
2024-06-09 09:09:27 +0900

    7.8 Beautiful Emails

---
+++ src/mail/mail.service.ts
@@ -0,0 +1,40 @@
+import got from 'got';
+import * as FormData from 'form-data';
+import { Inject, Injectable } from '@nestjs/common';
+import { CONFIG_OPTIONS } from 'src/common/common.constants';
+import { MailModuleOptions } from './mail.interfaces';
+
+@Injectable()
+export class MailService {
+  constructor(
+    @Inject(CONFIG_OPTIONS) private readonly options: MailModuleOptions,
+  ) {
+    this.sendEmail('testing', 'test');
+  }
+
+  private async sendEmail(subject: string, content: string) {
+    const form = new FormData();
+    form.append('from', `Excited User <mailgun@${this.options.domain}>`);
+    form.append('to', `kihoon.nam.dev@gmail.com`);
+    form.append('subject', subject);
+    form.append('template', 'verify-email');
+    form.append('v:code', 'asasas');
+    form.append('v:username', 'nico!!!');
+    const response = await got(
+      `https://api.mailgun.net/v3/${this.options.domain}/messages`,
+      {
+        https: {
+          rejectUnauthorized: false,
+        },
+        method: 'POST',
+        headers: {
+          Authorization: `Basic ${Buffer.from(
+            `api:${this.options.apiKey}`,
+          ).toString('base64')}`,
+        },
+        body: form,
+      },
+    );
+    console.log(response.body);
+  }
+}
