e43b38f75d7fe4f68f8f3bb3bdfcbc80bcdcc5f0
kihoonnamdev <kihoon.nam.dev@gmail.com>
2024-06-30 16:29:31 +0900

    12.12 getOrders part One

--- src/orders/orders.resolver.ts
+++ src/orders/orders.resolver.ts
@@ -1,4 +1,4 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver, Query } from '@nestjs/graphql';
 import { AuthUser } from 'src/auth/auth-user.decorator';
 import { Role } from 'src/auth/role.decorator';
 import { User } from 'src/users/entities/user.entity';
